bc89aba7de2b249097daa5ef43f96c58a2693e98
SwathiKannan <swathikakannan2007@gmail.com>
2026-08-13 19:49:59 +0530

    Day 3:Added Leetcode Solutions

---
+++ .leetcode/20.valid-parentheses.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=20 lang=java
+ *
+ * [20] Valid Parentheses
+ */
+
+// @lc code=start
+import java.util.*;
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' || c == '}' || c == ']') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
+            
+// @lc code=end
+
